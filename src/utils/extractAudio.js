03fc1e5616689729481e050aff8bdf1d64b60e2d
Youjin-cmd <y.kimiau@gmail.com>
2023-08-14 21:29:05 +0900

    Refactor. change extract fns to utils

---
+++ src/utils/extractAudio.js
@@ -0,0 +1,45 @@
+const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+const execFile = require("child_process").spawn;
+const path = require("path");
+
+const ensureFolderExists = require("../utils/ensureFolderExists");
+
+const SAVING_DIR_AUDIO = path.join(__dirname, "../../audio");
+ensureFolderExists(SAVING_DIR_AUDIO);
+
+exports.extractAudio = async (req) => {
+  const ffmpegAudio = execFile(ffmpegPath, [
+    "-i",
+    req.file.path,
+    "-vn",
+    "-ar",
+    "44100",
+    "-ac",
+    "2",
+    "-ab",
+    "192k",
+    "-f",
+    "mp3",
+    "-y",
+    path.join(SAVING_DIR_AUDIO, "audio.mp3"),
+  ]);
+
+  return new Promise(
+    (resolve) => {
+      ffmpegAudio.stdout.on("data", (x) => {
+        process.stdout.write(x.toString());
+      });
+      ffmpegAudio.stderr.on("data", (x) => {
+        process.stderr.write(x.toString());
+      });
+      ffmpegAudio.on("close", (code) => {
+        resolve({ success: true, code });
+        return true;
+      });
+    },
+    (reject) => {
+      console.error("Error occured extracting audio:", reject);
+      return false;
+    },
+  );
+};
---
+++ src/utils/extractDownscaledFrames.js
@@ -0,0 +1,43 @@
+const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+const execFile = require("child_process").spawn;
+const path = require("path");
+
+const ensureFolderExists = require("../utils/ensureFolderExists");
+
+const SAVING_DIR_DOWNSCALE = path.join(__dirname, "../../downscale");
+
+ensureFolderExists(SAVING_DIR_DOWNSCALE);
+
+exports.extractDownscaledFrames = async (req) => {
+  const ffmpegDownscale = execFile(ffmpegPath, [
+    "-i",
+    req.file.path,
+    "-vf",
+    "scale=50:-1",
+    "-r",
+    "10",
+    "-pix_fmt",
+    "pal8",
+    "-y",
+    path.join(SAVING_DIR_DOWNSCALE, "%04d.png"),
+  ]);
+
+  return new Promise(
+    (resolve) => {
+      ffmpegDownscale.stdout.on("data", (x) => {
+        process.stdout.write(x.toString());
+      });
+      ffmpegDownscale.stderr.on("data", (x) => {
+        process.stderr.write(x.toString());
+      });
+      ffmpegDownscale.on("close", (code) => {
+        resolve({ success: true, code });
+        return true;
+      });
+    },
+    (reject) => {
+      console.error("Error occured extracting downscaled frames:", reject);
+      return false;
+    },
+  );
+};
