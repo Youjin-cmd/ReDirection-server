55d7c87ee6a1d4fd384d9bd17b9efb30083d182c
Youjin-cmd <y.kimiau@gmail.com>
2023-08-16 16:19:24 +0900

    Setting. add aws config

--- src/constants/config.js
+++ src/constants/config.js
@@ -3,6 +3,10 @@ require("dotenv").config();
 const CONFIG = {
   PORT: process.env.PORT,
   CLIENT_URL: process.env.CLIENT_URL,
+  AWS_S3_REGION: process.env.AWS_S3_REGION,
+  AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
 };
 
 module.exports = CONFIG;
