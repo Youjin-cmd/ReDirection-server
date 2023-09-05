36475ade86557954400de58d0acdc58ea2d57c60
Youjin-cmd <y.kimiau@gmail.com>
2023-09-05 15:44:20 +0900

    Feat. add / to return 200

--- src/loaders/routers.js
+++ src/loaders/routers.js
@@ -1,6 +1,9 @@
 const videoRouter = require("../routes/video");
 
 async function routerLoader(app) {
+  app.get("/", (req, res) => {
+    res.status(200).send({ success: true });
+  });
   app.use("/video", videoRouter);
 }
 
