fd1fad15d1a470319bad7e45d86bec066d128d45
Youjin-cmd <y.kimiau@gmail.com>
2023-11-28 10:47:05 +0900

    Feat. add new router for GET'/trial/:id'

--- src/routes/video.js
+++ src/routes/video.js
@@ -4,9 +4,12 @@ const upload = require("../middleware/multer");
 const previewController = require("../controllers/preview.controller");
 const cropController = require("../controllers/crop.controller");
 const editController = require("../controllers/edit.controller");
+const trialController = require("../controllers/trial.controller");
 
 const { tryCatch } = require("../util/tryCatch");
 const { multerErrorHandler } = require("../util/multerErrorHandler");
+
+router.get("/trial/:id", tryCatch(trialController.createTrialPreview));
 
 router.post(
   "/preview",
